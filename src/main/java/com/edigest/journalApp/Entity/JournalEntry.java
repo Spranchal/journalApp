802798d833bc355f66f2a040086e80274920c6a1
Pranchal-Singh <spranchal864@gmail.com>
2026-01-17 20:26:06 +0530

    Properties | YML | Server | Running from terminal

--- src/main/java/com/edigest/journalApp/Entity/JournalEntry.java
+++ src/main/java/com/edigest/journalApp/Entity/JournalEntry.java
@@ -6,11 +6,15 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Document(collection = "journal_entries")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 // @Getter
 // @Setter
 public class JournalEntry {
--- src/main/java/com/edigest/journalApp/Entity/User.java
+++ src/main/java/com/edigest/journalApp/Entity/User.java
@@ -11,12 +11,16 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.mongodb.lang.NonNull;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 
 @Document(collection = "users")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
     @Id
     private ObjectId id;
     @Indexed(unique = true)
