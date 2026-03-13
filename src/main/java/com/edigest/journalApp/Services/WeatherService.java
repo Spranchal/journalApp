60c6243c09ef637b7584d6c900a4968636f0b601
Pranchal-Singh <spranchal864@gmail.com>
2026-03-13 21:16:10 +0530

    POST API using code, but not so related for weather api just seen the implementation

--- src/main/java/com/edigest/journalApp/Services/WeatherService.java
+++ src/main/java/com/edigest/journalApp/Services/WeatherService.java
@@ -1,8 +1,12 @@
 package com.edigest.journalApp.Services;
 
+import org.springframework.http.HttpHeaders;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.client.RestTemplate;
 
 import com.edigest.journalApp.api.response.WeatherResponse;
@@ -17,6 +21,16 @@ public class WeatherService {
 
     public WeatherResponse getWeather(String city) {
         String finalApi = API.replace("CITY", city).replace("API_KEY", apiKey);
+
+        // String requestBody = "{\r\n" + 
+        //                 "    \"userName\": \"Shyam\",\r\n" + 
+        //                 "    \"password\": \"Shyam\"\r\n" + 
+        //                 "}";
+        // HttpHeaders httpHeaders = new HttpHeaders();
+        // httpHeaders.set("key", "value"); 
+        // User user = User.builder().username("Shyam").password("Shyam").build();
+        // HttpEntity<User> httpEntity = new HttpEntity<>(user, httpHeaders);
+
         ResponseEntity<WeatherResponse>response = restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponse.class);
         WeatherResponse body = response.getBody();
         return body;
